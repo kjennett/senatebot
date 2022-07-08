1489911a522d1ab0871f10c9e335e1c2f58e915a
Seth Morgan <sethwritesthings@gmail.com>
2022-07-08 14:44:04 -0700

    chore: updates to autocomplete functions

--- src/functions/autocomplete/guildAutocomplete.js
+++ src/functions/autocomplete/guildAutocomplete.js
@@ -4,11 +4,14 @@ exports.guildAutocomplete = async (interaction, focused) => {
   const choices = await db.collection('guilds').find().sort({ name: 1 }).toArray();
 
   function isIncluded(value) {
-    if (focused.value === '') return true;
-    if (value.name.includes(focused.value)) return true;
-    if (value.name.toLowerCase().includes(focused.value)) return true;
-    if (value.abbr.includes(focused.value)) return true;
-    if (value.abbr.toLowerCase().includes(focused.value)) return true;
+    if (
+      focused.value === '' ||
+      value.name.includes(focused.value) ||
+      value.name.toLowerCase().includes(focused.value) ||
+      value.abbr.includes(focused.value) ||
+      value.abbr.toLowerCase().includes(focused.value)
+    )
+      return true;
     return false;
   }
 
--- src/functions/autocomplete/tierAutocomplete.js
+++ src/functions/autocomplete/tierAutocomplete.js
@@ -4,10 +4,14 @@ exports.tierAutocomplete = async (interaction, focused) => {
   const choices = await db.collection('tiers').find().sort({ number: 1 }).toArray();
 
   function isIncluded(value) {
-    if (focused.value === '') return true;
-    if (value.number === focused.value) return true;
-    if (`${value.number}` === `${focused.value}`) return true;
-    if (`${focused.value}`.includes(`${value}`)) return true;
+    if (
+      focused.value === '' ||
+      value.name.includes(focused.value) ||
+      value.name.toLowerCase().includes(focused.value) ||
+      value.abbr.includes(focused.value) ||
+      value.abbr.toLowerCase().includes(focused.value)
+    )
+      return true;
     return false;
   }
 
