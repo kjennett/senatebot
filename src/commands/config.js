58be94766b279e2efd4c26e00ee699b4b57b5809
Seth Morgan <sethwritesthings@gmail.com>
2022-07-22 09:25:14 -0700

    fix: inaccurate count of purged members using config order66 command

--- src/commands/config.js
+++ src/commands/config.js
@@ -110,28 +110,38 @@ module.exports = {
       await interaction.deferReply();
 
       const allMembers = await interaction.guild.members.fetch();
-      const hasPGM = await allMembers.filter(m => m.roles.cache.has(config.roles.potentialGuildMember));
-      const pgmOnly = await hasPGM.filter(m => m.roles.cache.size === 2);
-      const purgeableMembers = pgmOnly.filter(m => m.joinedTimestamp < Date.now() - 12096e5);
-      purgeableMembers.sort((a, b) => a.joinedTimestamp - b.joinedTimestamp);
+      const eligible = await allMembers.filter(m => {
+        if (
+          m.roles.cache.has(config.roles.potentialGuildMember) &&
+          m.roles.cache.size === 2 &&
+          m.joinedTimestamp < Date.now() - 12096e5
+        )
+          return true;
+        if (m.roles.cache.size === 1 && m.joinedTimestamp < Date.now() - 12096e5) return true;
+        return false;
+      });
 
       if (await !interaction.options.getBoolean('doit')) {
-        const purgeList = [];
-        purgeableMembers.forEach(m =>
-          purgeList.push(`User: ${m.displayName} | Joined: ${new Date(m.joinedTimestamp).toLocaleDateString()}`)
+        const eligibleList = [];
+        eligible.forEach(m =>
+          eligibleList.push(
+            `User: ${m.displayName} | Joined: ${new Date(m.joinedTimestamp).toLocaleDateString()} | Roles: ${
+              m.roles.cache.size - 1
+            }`
+          )
         );
 
-        if (purgeList.length) {
+        if (eligibleList.length) {
           const embed = new MessageEmbed({
             title: 'Purgeable Users',
-            description: `The following ${purgeList.length} members:\n - __Only__ have the Potential Guild Member role\n - Joined the server more than 14 days ago\n - Do not have an active recruitment thread\n\nTo remove these users from the server, run this command again with the DOIT option set to True.`,
+            description: `The following ${eligibleList.length} members:\n - __Only__ have the Potential Guild Member role, or have no roles\n - Joined the server more than 14 days ago\n - Do not have an active recruitment thread\n\nTo remove these users from the server, run this command again with the DOIT option set to True.`,
           });
-          await writeFileSync('./purgelist.txt', purgeList.join('\n'));
+          await writeFileSync('./purgelist.txt', eligibleList.join('\n'));
           const fileAttachment = new MessageAttachment('./purgelist.txt');
           await interaction.editReply({
             embeds: [embed],
-            files: [fileAttachment],
           });
+          await interaction.followUp({ files: [fileAttachment] });
           await unlinkSync('./purgelist.txt');
         } else {
           const embed = new MessageEmbed({
@@ -142,18 +152,22 @@ module.exports = {
         }
       } else {
         const purgedList = [];
-        if (purgeableMembers.size) {
-          purgeableMembers.forEach(async m => {
+        const failedList = [];
+        if (eligible.size) {
+          eligible.forEach(async m => {
             try {
               await m.send(
                 'You have been automatically removed from ΞTHE SENATEΞ Alliance Discord Server, as you have not been granted a role within 14 days of joining the server.\nIf you believe this to be in error, please rejoin the server using the following link:\n\nhttp://discord.thesenate.gg\n\nΞThe SenateΞ wishes you good fortune in your SWGOH adventures - may the Force be with you, always!'
               );
-              await m.kick('Purged: 14 days without receiving a role.');
             } catch (e) {
-              await m.kick('Purged: 14 days without receiving a role.');
+              await failedList.push(m.displayName);
             }
 
-            await purgedList.push(`User: ${m.displayName} | Joined: ${new Date(m.joinedTimestamp).toLocaleDateString()}`);
+            await m.kick('Purged: 14 days without receiving a role.');
+            await purgedList.push(`User: ${m.displayName} | Joined: ${new Date(
+              m.joinedTimestamp
+            ).toLocaleDateString()} | Roles: ${m.roles.cache.size - 1}
+            `);
           });
 
           await writeFileSync('./purgelist.txt', purgedList.join('\n'));
@@ -166,8 +180,13 @@ module.exports = {
           await interaction.editReply('https://tenor.com/view/palpatine-star-wars-emperor-do-it-go-for-it-gif-17446081');
           await interaction.followUp({
             embeds: [embed],
-            files: [fileAttachment],
           });
+          await interaction.followUp({ files: [fileAttachment] });
+          const failedEmbed = new MessageEmbed({
+            title: `The following members were not able to be notified of their removal:`,
+            description: failedList.join(', '),
+          });
+          await interaction.followUp({ embeds: [failedEmbed] });
 
           await unlinkSync('./purgelist.txt');
         } else {
