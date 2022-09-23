85c6c4e217d7354b273e9e32c8d5388d32024e3b
Seth Morgan <sethwritesthings@gmail.com>
2022-09-23 16:15:51 -0700

    oof

--- src/functions/fetchPlayerData.js
+++ src/functions/fetchPlayerData.js
@@ -1,18 +1,16 @@
 const axios = require('axios').default;
 const rateLimit = require('axios-rate-limit');
 
-// Fetch account data from SWGOH.gg
 exports.fetchGG = async allycode => {
   try {
     const response = await axios.get(`http://api.swgoh.gg/player/${allycode}`);
-    if (response.status !== 200) return false;
-    return true;
+    if (response.status !== 200) return null;
+    return response.data;
   } catch (e) {
-    return false;
+    return null;
   }
 };
 
-// Fetch guild profile data from SWGOH.GG
 exports.fetchGgGuild = async guildId => {
   try {
     const response = await axios.get(`http://api.swgoh.gg/guild-profile/${guildId}`);
@@ -24,7 +22,6 @@ exports.fetchGgGuild = async guildId => {
   }
 };
 
-// Fetch mod data from OmegaBot
 exports.fetchOmega = async allycode => {
   try {
     const response = await axios.get(`${process.env.OMEGA}${allycode}`);
@@ -39,11 +36,25 @@ exports.fetchOmega = async allycode => {
 // Globally limits queries to the SWGOH.HELP premium client (currently 2 requests / second)
 const limited = rateLimit(axios.create(), { maxRPS: 2 });
 
-// Fetch account data from SWGOH.Help
 exports.fetchHelp = async allycode => {
   try {
     const response = await limited.get(`${process.env.SWGOHHELP}player/${allycode}`);
     if (response.data.error) return null;
+    return response.data;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+};
+
+exports.fetchComlink = async allycode => {
+  try {
+    const response = await axios.post(process.env.COMLINK, {
+      payload: {
+        allyCode: allycode,
+      },
+      enums: true,
+    });
     return response.data;
   } catch (e) {
     console.error(e);
