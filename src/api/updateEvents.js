c82b6865cc4dae0aa40dbbaab678ba84e744109b
Seth Morgan <sethwritesthings@gmail.com>
2022-10-08 14:29:31 -0700

    add extra parsing for events calendar

--- src/api/updateEvents.js
+++ src/api/updateEvents.js
@@ -4,7 +4,7 @@ const ical = require('node-ical');
 async function updateEvents() {
   const calendar = await ical.async.fromURL('https://swgohevents.com/ical');
   await db.collection('events').deleteMany();
-  await db.collection('events').insertMany(calendar);
+  await db.collection('events').insertMany(Object.values(calendar));
 }
 
 module.exports = async () => {
