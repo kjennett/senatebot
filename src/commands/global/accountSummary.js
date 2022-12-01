5f3694a54353edd9315c47ee2db9268a83abe8e2
Seth Morgan <sethwritesthings@gmail.com>
2022-12-01 09:43:58 -0800

    moved account summary to global

--- src/commands/global/accountSummary.js
+++ src/commands/global/accountSummary.js
@@ -7,18 +7,10 @@ module.exports = {
   enabled: true,
 
   data: new SlashCommandBuilder()
-    .setName('account')
-    .setDescription('Fetch game data for a specific player')
-    .addSubcommand(s1 =>
-      s1
-        .setName('summary')
-        .setDescription('Display a summary showing the current state of an account.')
-        .addStringOption(option =>
-          option
-            .setName('allycode')
-            .setDescription('Ally code or non-vanity SWGOH.gg link for the account.')
-            .setRequired(true)
-        )
+    .setName('accountsummary')
+    .setDescription('Display a summary showing the current state of an account.')
+    .addStringOption(option =>
+      option.setName('allycode').setDescription('Ally code or non-vanity SWGOH.gg link for the account.').setRequired(true)
     ),
 
   async execute(i) {
