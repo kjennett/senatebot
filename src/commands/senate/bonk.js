1c8152f69bfaac4e7da16bc3846d95127d4fe0d0
Seth Morgan <sethwritesthings@gmail.com>
2022-12-02 10:40:26 -0800

    duh bonk

--- src/commands/senate/bonk.js
+++ src/commands/senate/bonk.js
@@ -13,8 +13,7 @@ module.exports = {
 
     await i.reply({ content: 'SenateBONK Activated!', ephemeral: true });
 
-    const { message } = i.targetMessage;
-    console.log(`Bonking message with id: ${message.id}`);
+    const message = i.targetMessage;
     await message.channel.send('https://tenor.com/view/bonk-mega-bonk-bonk-dog-bonkers-bonk-anime-gif-24565990');
 
     let bonkCount = 1;
