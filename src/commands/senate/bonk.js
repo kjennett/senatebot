73e477dc8093337b3031805b393874e28c2ecc89
Seth Morgan <sethwritesthings@gmail.com>
2022-12-02 13:56:31 -0800

    bonk updates

--- src/commands/senate/bonk.js
+++ src/commands/senate/bonk.js
@@ -11,24 +11,20 @@ module.exports = {
       return i.reply({ content: `BONK! You don't have permission to use SenateBONK!`, ephemeral: true });
     }
 
-    await i.reply({ content: 'SenateBONK Activated!', ephemeral: true });
+    await i.reply({ content: 'BONKED!', ephemeral: true });
 
     const message = i.targetMessage;
-    await message.reply('https://tenor.com/view/bonk-mega-bonk-bonk-dog-bonkers-bonk-anime-gif-24565990');
-
     await message.member.roles.add(await i.member.guild.roles.fetch('1048311613284941854'));
 
     let bonkCount = 1;
     const bonkedUser = await db.collection('bonks').findOne({ id: message.member.id });
     if (bonkedUser) bonkCount = bonkedUser.count + 1;
-
     if (!bonkedUser) await db.collection('bonks').insertOne({ id: message.member.id, count: bonkCount });
     if (bonkedUser) await db.collection('bonks').findOneAndUpdate({ id: message.member.id }, { $set: { count: bonkCount } });
 
     console.log(`Bonked ${message.member.displayName}. Bonk Count: ${bonkCount}`);
-
-    return message.member.send(
-      `BONK! The following message has been bonked:\n\n${message.url}\n\nYou have been bonked ${bonkCount} times. Don't make SenateBONK send you to horny jail!`
+    return message.reply(
+      `This message has been BONKed!\n\nYou have been bonked a total of ${bonkCount} times.\n\nDon't make SenateBONK send you to horny jail!\nhttps://tenor.com/view/bonk-mega-bonk-bonk-dog-bonkers-bonk-anime-gif-24565990`
     );
   },
 };
