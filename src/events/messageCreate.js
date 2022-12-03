9a470fd36649649b7ae7aa694296669806e847c0
Seth Morgan <sethwritesthings@gmail.com>
2022-12-03 08:22:19 -0800

    fix

--- src/events/messageCreate.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'messageCreate',
-
-  async execute(message) {
-    if (message.content.toLowerCase().includes('circle')) {
-      await message.react('👀');
-    }
-  },
-};
