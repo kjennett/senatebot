bc7f676ead81d5716fbcbd92f60f70d2af7ab4e4
Seth Morgan <sethwritesthings@gmail.com>
2022-12-09 17:39:34 -0800

    fix

--- src/commands/senate/invite.js
+++ src/commands/senate/invite.js
@@ -12,7 +12,7 @@ module.exports = {
       return i.editReply('You must be a Guild Officer to use this command.');
 
     await i.editReply(
-      `Use this link to invite SenateBot to your server. You must have admin permissions on your server to use the link. __Please do not share outside The Senate.__\n\n[Invite SenateBot](${config.invite})`
+      `You must have admin permissions on your server to use the link. The link will invite SenateBot to your server and grant it Admin permissions.\n__Please do not share outside The Senate.__\n[Invite SenateBot](${config.invite})`
     );
   },
 };
