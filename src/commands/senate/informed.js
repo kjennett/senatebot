b58bc4df4d9e8e68e96aaf57a8cd3a1e61efe825
Seth Morgan <sethwritesthings@gmail.com>
2022-12-17 11:17:17 -0800

    fix

--- src/commands/senate/informed.js
+++ src/commands/senate/informed.js
@@ -18,7 +18,7 @@ module.exports = {
       await i.channel.send(
         ':white_check_mark: **This player has been informed of their transfer - you may now contact them and pitch your guild!** :white_check_mark:'
       );
-      await i.deleteReply();
+      await i.editReply(':white_check_mark:');
     } else {
       await i.editReply('Please use this command in a Trade Federation transfer thread.');
     }
