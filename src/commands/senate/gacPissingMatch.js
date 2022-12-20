496f723867335481b085aa71e4c16818831b32a8
Seth Morgan <sethwritesthings@gmail.com>
2022-12-20 10:34:31 -0800

    test

--- src/commands/senate/gacPissingMatch.js
+++ src/commands/senate/gacPissingMatch.js
@@ -1,5 +1,6 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { db } = require('../../database');
+const { fetchGuildProfile, fetchAccount } = require('../../api/swgohgg');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -14,17 +15,48 @@ module.exports = {
     if (i.member.id !== process.env.OWNER)
       return i.deferReply('Only the bot administrator may run this command (for now!)');
 
+    /** Total alliance member count for all guilds */
     let totalAllianceMembers = 0;
+    /** Total GP of all alliance guilds */
     let totalAlliancePower = 0;
     const allGuilds = await db.collection('guilds').find().toArray();
-    console.log(allGuilds.length);
     for (const guild of allGuilds) {
       totalAllianceMembers += guild.members;
       totalAlliancePower += guild.gp;
     }
 
-    return i.editReply(
-      `Total alliance members: ${totalAllianceMembers} | Total alliance galactic power: ${totalAlliancePower.toLocaleString()}`
+    const totalAllianceMillionGP = totalAlliancePower / 1000000;
+
+    let totalScannedMembers = 0;
+    let totalAllianceSkillRating = 0;
+    let guildCount = 1;
+    for (const guild of allGuilds) {
+      await i.editReply(`Scanning guild members - Guild ${guildCount} of ${allGuilds.length}`);
+      const guildProfile = await fetchGuildProfile(guild.gg);
+
+      for (const member of guildProfile.data.members) {
+        if (!member.ally_code) continue;
+
+        const accountData = await fetchAccount(member.ally_code);
+        if (!accountData) continue;
+
+        if (accountData.data.skill_rating) {
+          totalAllianceSkillRating += accountData.data.skill_rating;
+          totalScannedMembers += 1;
+        }
+      }
+
+      guildCount++;
+    }
+
+    await i.editReply(
+      `_ _\nTotal Alliance Members: ${totalAllianceMembers.toLocaleString()}\nTotal Alliance GP: ${totalAlliancePower.toLocaleString()}\nTotal Alliance Skill Rating: ${
+        totalAllianceSkillRating.toLocaleString
+      }\nAlliance Average Skill Rating: ${Math.floor(
+        totalAllianceSkillRating / totalScannedMembers
+      )}\nAlliance Average Skill Rating Per 1M GP: ${(
+        totalAllianceSkillRating / totalAllianceMillionGP
+      ).toFixed(2)}`
     );
   },
 };
