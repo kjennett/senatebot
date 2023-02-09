66c242eab9a5cfff1f964458e4a5cc475e337f72
Seth Morgan <sethwritesthings@gmail.com>
2023-02-09 14:51:42 -0800

    add debug to gacphase embed

--- src/commands/global/gacPhase.js
+++ src/commands/global/gacPhase.js
@@ -9,16 +9,17 @@ module.exports = {
   async execute(i) {
     await i.deferReply();
 
-    const allPhases = await db.collection('events').find({}).toArray();
+    const allPhases = await db.collection('events').find({}).sort({ name: 1, season: 1 }).toArray();
     const currentPhase = allPhases.filter(phase => {
       phase.start <= Date.now() && phase.end >= Date.now();
     });
+
     if (!currentPhase.length === 1)
       return i.editReply(
         'No applicable GAC phase was found; there is either no GAC event active or the events calendar is out of date.'
       );
 
-    const embed = new EmbedBuilder().setTitle(currentPhase.name).setDescription(currentPhase.season);
+    const embed = new EmbedBuilder().setTitle(currentPhase.name ?? 'Test').setDescription(currentPhase.season ?? 'Test');
     return i.editReply({ embeds: [embed] });
   },
 };
